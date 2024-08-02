3148107fca8dbc7f6b76af630932c25b37e1fd73
Malins062 <6328673@gmail.com>
2024-08-02 22:42:11 +0300

    feat: add base settings

---
+++ api/app/config.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    messages: str = "/messages"
+    message: str = "/message"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class DatabaseConfig(BaseModel):
+    mongo_uri: str = "mongodb://localhost:27017/"
+    mongo_db: str = "messages"
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+
+
+settings = Settings()

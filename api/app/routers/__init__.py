65cbcc291a2fabc595560dcbcab2688aed7999e7
Malins062 <6328673@gmail.com>
2024-08-02 22:41:42 +0300

    feat: add routers

--- api/app/routers/__init__.py
+++ api/app/routers/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from core.config import settings
+from ..config import settings
 from .api_v1 import router as router_api_v1
 
 router = APIRouter(
---
+++ api/app/routers/api_v1/__init__.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from ...config import settings
+
+from .messages import router as users_router
+
+router = APIRouter(
+    prefix=settings.api.v1.prefix,
+)
+router.include_router(
+    messages_router,
+    prefix=settings.api.v1.messages,
+)

29e894098c906fa15d8bc61dcda04d11c1a9bf6f
Malins062 <6328673@gmail.com>
2024-08-03 10:19:30 +0300

    refactor: routers params

--- api/app/routers/api_v1/__init__.py
+++ api/app/routers/api_v1/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from ...config import settings
 
-from .messages import router as users_router
+from .messages import router as messages_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
---
+++ api/app/routers/api_v1/messages.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from fastapi.routing import APIRoute
+
+from ...config import settings
+from ...db.crud.messages import get_messages, create_message
+
+routes = [
+    APIRoute(
+        path=settings.api.v1.messages,
+        endpoint=get_messages,
+        methods=["GET"],
+    ),
+    APIRoute(
+        path=settings.api.v1.message,
+        endpoint=create_message,
+        methods=["POST"],
+    ),
+]
+
+router = APIRouter(
+    routes=routes,
+    tags=["Messages"],
+)

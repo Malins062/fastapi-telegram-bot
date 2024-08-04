89c28929bb12a7e9424319121cb475cbffa57c9b
ALEXEY MALINOVSKIY <6328673@gmail.com>
2024-08-04 17:58:02 +0300

    refactor: models db

--- bot/app/db/engine.py
+++ bot/app/db/engine.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.storage.redis import RedisStorage
 
-from ...config import settings
+from config import settings
 
 # Redis storage
 redis_storage = RedisStorage.from_url(settings.db.redis_url)
